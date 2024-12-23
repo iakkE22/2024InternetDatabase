061d1674df56975d4e10168460bb9b6a7803e1bc
mu_mian <2220634014@qq.com>
2024-12-23 15:35:36 +0800

    🐞 fix:修复一个bug

--- music_list_page/music-project/src/router/index.js
+++ music_list_page/music-project/src/router/index.js
@@ -16,7 +16,7 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/homepage.vue'),
       meta: {
-        showNavBar: true
+        showNavBar: false
       }
     },
     {
@@ -24,7 +24,7 @@ const router = createRouter({
         name: 'aboutus',
         component: () => import('../views/AboutUsView.vue'),
         meta: {
-          showNavBar: true
+          showNavBar: false
         }
       },
     {
@@ -32,33 +32,29 @@ const router = createRouter({
         name: 'userpage',
         component: () => import('../views/UserPageView.vue'),
         meta: {
-          showNavBar: true
+          showNavBar: false
         }
       },
-      {
-        path: "/video",
-        name: "videoList",
-        component: () => import("../views/VideoListView.vue"),
-      },
-      {
-        path: "/video/:id",
-        name: "videoDetail",
-        component: () => import("../views/VideoDetailView.vue"),
-      },
     {
-      path: '/list',
+      path: '/playlist',
       name: 'songlist',
       component: () => import('../views/songListView.vue'),
       meta: {
         showNavBar: true
       }
     },
+    {
+      path: "/video",
+      name: "videoList",
+      component: () => import("../views/VideoListView.vue"),
+    },
+    {
+      path: "/video/:id",
+      name: "videoDetail",
+      component: () => import("../views/VideoDetailView.vue"),
+    },
   ]
 })
-router.beforeEach((to, from, next) => {
-  console.log("目标路由 meta 信息：", to.meta);
-  next();
-});
 
 // router.beforeEach((to, from, next) => {
 //   const Username = sessionStorage.getItem('Username')
---
+++ music_list_page/music-project/src/views/test.vue
@@ -0,0 +1,10 @@
+{
+    path: "/video",
+    name: "videoList",
+    component: () => import("../views/VideoListView.vue"),
+  },
+  {
+    path: "/video/:id",
+    name: "videoDetail",
+    component: () => import("../views/VideoDetailView.vue"),
+  },
